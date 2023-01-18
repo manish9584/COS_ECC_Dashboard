f70bbca34a2d8b864bd8dc1b1ef4a2418499cb2a
Manish Patil <57797058+manish9584@users.noreply.github.com>
2023-01-18 21:16:34 +0530

    Select Dashboard Section With Login.

--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -7,9 +7,13 @@ test ('Dash', async function({browser})
     await page.goto("https://test-ecc.tangandcompany.com/Account/Login?returnUrl=%2f")
 
     // Login With Creadential in Test Site.
-
     await page.locator('#LoginId').type("manish");
     await page.locator('#Password').type("Manish@9584")
     await page.click('.btn-green')
-
+    
+    //GoTo Dashboard Section
+    await page.locator('.fa-caret-down').nth(13).click();
+    await page.locator("[href ='/angular/ecc/dashboard']").click();
+    //await page.locator('.ng-value-container').nth(0).click();
+    await page.pause();
 })
