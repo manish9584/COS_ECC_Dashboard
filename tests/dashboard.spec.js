e649e8bede0fd6e56d1e56fc7117edf713835887
Manish Patil <57797058+manish9584@users.noreply.github.com>
2023-01-18 20:16:53 +0530

    Login Code
    Login with valid credentials in test site.

--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -1,1 +1,15 @@
-const test = require('@playwright/test')
+const {test} = require('@playwright/test')
+
+test ('Dash', async function({browser})
+{
+    const context = await browser.newContext()
+    const page = await context.newPage()
+    await page.goto("https://test-ecc.tangandcompany.com/Account/Login?returnUrl=%2f")
+
+    // Login With Creadential in Test Site.
+
+    await page.locator('#LoginId').type("manish");
+    await page.locator('#Password').type("Manish@9584")
+    await page.click('.btn-green')
+
+})
