fc230eb82a0d1ef7a416a6f6d28f1b25961172d2
manish9584 <57797058+manish9584@users.noreply.github.com>
2023-02-03 12:40:47 +0530

    Redirect To Dashboard
    Remove unnecessary code.

--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -4,30 +4,14 @@ test ('Dash', async function({browser})
 {
     const context = await browser.newContext()
     const page = await context.newPage()
-    await page.goto("https://test-ecc.tangandcompany.com/Account/Login?returnUrl=%2f")
+    await page.goto("http://localhost:54632/Account/Login?returnUrl=%2f")
 
     // Login With Creadential in Test Site.
     await page.locator('#LoginId').type("manish");
-    await page.locator('#Password').type("Manish@9584")
+    await page.locator('#Password').type("asap")
     await page.click('.btn-green')
     
     //GoTo Dashboard Section
-    await page.locator('.fa-caret-down').nth(13).click();
-    //await page.pause();
-    await page.locator("[href*='angular/ecc/dashboard']").click();
-    //await page.pause();
-
-    await page.locator(".ng-input").nth(0).click();
-    //await page.pause();
-    await page.locator('.ng-option').nth(13).click();
-    //await page.pause();
+    await page.goto("http://localhost:54632/angular/ecc/dashboard/mydashboard")
     
 })
-//const {test} = require('@playwright/test')
-
-//test ('Dashboard_Main', function({browser})
-// {
-//     const context = browser.newContext()
-//     const page = context.newPage()
-//     page.goto("https://test-ecc.tangandcompany.com/Account/Login?returnUrl=%2f")
-// })
