c7b6ff6bcef090a3091a60350974f32717a745b0
manish9584 <57797058+manish9584@users.noreply.github.com>
2023-02-03 12:53:43 +0530

    URL Setup for Local & Test Site

--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -4,14 +4,19 @@ test ('Dash', async function({browser})
 {
     const context = await browser.newContext()
     const page = await context.newPage()
-    await page.goto("http://localhost:54632/Account/Login?returnUrl=%2f")
+    await page.goto("https://test-ecc.tangandcompany.com/Account/Login?returnUrl=%2f") //Test-Site
+    //await page.goto("http://localhost:54632/Account/Login?returnUrl=%2f") //Local
 
     // Login With Creadential in Test Site.
     await page.locator('#LoginId').type("manish");
-    await page.locator('#Password').type("asap")
+    await page.locator('#Password').type("Manish@9584")
+    
+    // Login With Creadential in Local Site.
+    // await page.locator('#LoginId').type("manish");
+    // await page.locator('#Password').type("asap")
     await page.click('.btn-green')
     
     //GoTo Dashboard Section
-    await page.goto("http://localhost:54632/angular/ecc/dashboard/mydashboard")
-    
+    await page.goto("https://test-ecc.tangandcompany.com/angular/ecc/dashboard/mydashboard") //Test-Site
+    //await page.goto("http://localhost:54632/angular/ecc/dashboard/mydashboard") //Local
 })
